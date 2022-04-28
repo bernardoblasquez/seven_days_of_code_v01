c7a9f2cd7e916f0fe639eb4be8b9341fe57f9122
Bernardo <bernardo.bt@gmail.com>
2022-04-28 08:23:37 -0300

    refactor: organização do CSS usando BEM

--- casa-verde/src/components/Header/Menu/Menu.tsx
+++ casa-verde/src/components/Header/Menu/Menu.tsx
@@ -2,13 +2,23 @@ import styles from './style.module.scss'
 
 export function Menu() {
    return(
-      <nav className="">
-         <ul className={styles['menu-list']}>
-            <li><a href="">Como fazer</a></li>
-            <li><a href="">Ofertas</a></li>
-            <li><a href="">Depoimentos</a></li>
-            <li><a href="">Vídeos</a></li>
-            <li><a href="">Meu Carrinho</a></li>
+      <nav className="menu">
+         <ul className={styles['menu__list']}>
+            <li className={styles['menu__item']}>
+               <a href="#" className={styles['menu__link']}>Como fazer</a>
+            </li>
+            <li className={styles['menu__item']}>
+               <a href="" className={styles['menu__link']}>Ofertas</a>
+            </li>
+            <li className={styles['menu__item']}>
+               <a href="" className={styles['menu__link']}>Depoimentos</a>
+            </li>
+            <li className={styles['menu__item']}>
+               <a href="" className={styles['menu__link']}>Vídeos</a>
+            </li>
+            <li className={styles['menu__item']}>
+               <a href="" className={styles['menu__link']}>Meu Carrinho</a>
+            </li>
          </ul>
       </nav>
    )
