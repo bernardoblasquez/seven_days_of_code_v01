ce0ba4c8ba62b0aacc096c1d02512396023d0c68
Bernardo <bernardo.bt@gmail.com>
2022-04-29 10:02:41 -0300

    feature: criação da estrutura do componente da newsletter

---
+++ casa-verde/src/components/MainContent/Newsletter/Newsletter.tsx
@@ -0,0 +1,27 @@
+import styles from './style.module.scss'
+
+export function Newsletter (){
+   return (
+      <section className={styles["newsletter"]}>
+         <h2 className={styles["newsletter__title"]}>
+               
+            Sua casa com as
+            <span className={styles["newsletter__main-title"]}>
+               melhores plantas
+            </span>
+            
+         </h2>
+         <p className={styles["newsletter__description"]}>
+         Encontre aqui uma vasta seleção de plantas para decorar a 
+         sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. 
+         Entre com seu e-mail e assine nossa newsletter para saber 
+         das novidades da marca.
+         </p>
+         <form action="" className={styles["newsletter__subscribe-form"]}>
+            <input type="text" />
+            <button>Assinar newsletter</button>
+         </form>
+
+      </section>
+   )
+}
